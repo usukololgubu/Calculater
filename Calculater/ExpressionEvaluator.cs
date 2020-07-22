a158eac861401691d4e76a26619476a34b2b78ed
usukololgubu <a.pryah@gmail.com>
2020-07-22 14:21:35 +0700

    Calculater refactoring
    Calculater should only wraps the Calculator app and do nothing else. Move all the logic related to expressions from the Calculater to a separate class.

---
+++ Calculater/ExpressionEvaluator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculater
+{
+    public class ExpressionEvaluator
+    {
+        public static T Evaluate<T>(string expression, Func<string, T> performBinaryOperation)
+        {
+            var stack = new Stack<string>();
+            var current = "";
+            foreach (var c in expression)
+            {
+                switch (c)
+                {
+                    case '(':
+                        stack.Push(current);
+                        current = "";
+                        break;
+                    case ')':
+                        current = stack.Pop() + performBinaryOperation(current);
+                        break;
+                    default:
+                        current += c;
+                        break;
+                }
+            }
+
+            return performBinaryOperation(current);
+        }
+    }
+}
