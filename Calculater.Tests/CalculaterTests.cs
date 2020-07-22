43eb9765b1067494231bafd6cd24e50906a3d333
usukololgubu <a.pryah@gmail.com>
2020-07-22 14:13:35 +0700

    Add calculator tests

---
+++ Calculater.Tests/CalculaterTests.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Calculater.Tests
+{
+    [TestFixture]
+    public class CalculaterTests
+    {
+        [Test]
+        public void TestOpenClose()
+        {
+            var calc = new Calculater();
+
+            Assert.NotNull(Process.GetProcesses().FirstOrDefault(p => p.ProcessName == "Calculator"));
+            
+            calc.Close();
+            Thread.Sleep(500); // wait a bit to be sure that Calculator is closed
+
+            Assert.Null(Process.GetProcesses().FirstOrDefault(p => p.ProcessName == "Calculator"));
+        }
+
+        [Test]
+        public void InputTest()
+        {
+            var calc = new Calculater();
+
+            var result = calc.Calculate("1234567890");
+            Assert.AreEqual(1234567890, result);
+            
+            calc.Close();
+        }
+    }
+}
